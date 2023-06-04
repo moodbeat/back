2c2ba170a602984c11c9301255ec346f3dc4abec
Aleksandr Usolcev <alexusolev@gmail.com>
2023-06-04 21:28:24 +0300

    feat(bot): auth/tokens requests errors

--- bot/handlers/auth.py
+++ bot/handlers/auth.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import requests
 from aiogram import Router, types
 from aiogram.filters.command import Command
@@ -36,7 +38,33 @@ async def save_user_data(message: types.Message, state: FSMContext):
     email = data.pop('email')
     password = data.pop('password')
     telegram_id = message.from_user.id
-    tokens = get_auth_tokens(email, password)
+    data = {
+        'email': email,
+        'password': password
+    }
+
+    try:
+        response = await requests.post(
+            config.base_endpoint + 'auth/jwt/create/', json=data
+        )
+        response.raise_for_status()
+        tokens = response.json()
+
+    except requests.exceptions.ConnectionError:
+        return await message.answer(
+            'Не удалось соединиться с сервером авторизации.'
+        )
+
+    except requests.exceptions.RequestException:
+
+        if response.status_code == HTTPStatus.UNAUTHORIZED:
+            await message.answer(
+                'Не найдено учетной записи с указанными данными.'
+            )
+        else:
+            await message.answer('Какие то проблемы.')
+
+        return
 
     await save_auth_data(
         telegram_id=telegram_id,
@@ -45,18 +73,3 @@ async def save_user_data(message: types.Message, state: FSMContext):
         refresh_token=tokens.get('refresh')
     )
     await message.answer('Вы успешно авторизованы.')
-
-
-def get_auth_tokens(email, password):
-    data = {
-        'email': email,
-        'password': password
-    }
-
-    try:
-        response = requests.post(
-            config.base_endpoint + 'auth/jwt/create/', json=data
-        )
-        return response.json()
-    except requests.exceptions.RequestException as error:
-        return error
