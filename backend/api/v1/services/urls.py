d41d9d074c8ff6a44a08b8b3f189200ce57c99d2
Aleksandr Usolcev <alexusolev@gmail.com>
2023-06-08 21:44:54 +0300

    fix(services): hide log endpoint when service off

--- backend/api/v1/services/urls.py
+++ backend/api/v1/services/urls.py
@@ -1,11 +1,15 @@
+from django.conf import settings
 from django.urls import re_path
 
 from . import views
 
-urlpatterns = [
-    re_path(
-        r'^download-log/?$',
-        views.DownloadLogView.as_view(),
-        name='download_log'
-    ),
-]
+urlpatterns = []
+
+if settings.DEV_SERVICES:
+    urlpatterns += (
+        re_path(
+            r'^download-log/?$',
+            views.DownloadLogView.as_view(),
+            name='download_log'
+        ),
+    )
