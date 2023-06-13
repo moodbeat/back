ac391571f46bc739d1dfce731aab8e4d9912678a
Aleksandr Usolcev <alexusolev@gmail.com>
2023-06-13 18:57:50 +0300

    feat(metrics): mbi survey type base calc

--- backend/metrics/models.py
+++ backend/metrics/models.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+from math import sqrt
 
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
@@ -365,6 +366,26 @@ class CompletedSurvey(models.Model):
         )
         return super().clean()
 
+    def set_mental_state(self, result_in_persent):
+
+        level = 1
+
+        if self.survey.min_range is not None:
+
+            if result_in_persent in range(self.survey.min_range):
+                level = 1
+            elif result_in_persent in range(
+                self.survey.min_range, self.survey.max_range
+            ):
+                level = 2
+            elif result_in_persent in range(self.survey.max_range, 101):
+                level = 3
+
+        mental_state = MentalState.objects.filter(level=level).first()
+        self.mental_state = mental_state
+        self.employee.mental_state = mental_state
+        self.employee.save()
+
     def save(self, *args, **kwargs):
         """При создании объекта интерпретирует значение результата в текст.
 
@@ -372,46 +393,110 @@ class CompletedSurvey(models.Model):
         в модели `Survey` определяет дату следующей попытки прохождения
         опроса.
         """
-    #     result_in_persent = (
-    #         self.positive_value / self.survey.questions.count() * 100
-    #     )
-    #     if result_in_persent in range(11):
-    #         state = self.ResultInterpretation.NORM
-    #         level = 1
-    #     elif result_in_persent in range(11, 69):
-    #         state = self.ResultInterpretation.HARD
-    #         level = 2
-    #     elif result_in_persent in range(69, 101):
-    #         state = self.ResultInterpretation.CRIT
-    #         level = 3
+        # временный костыль, сделаю по нормальному
+        if self.survey.type.slug == 'yn':
+            values = [item['variant_value'] for item in self.results]
+            result_in_persent = (
+                values.count(1) / self.survey.questions.count() * 100
+            )
+            self.set_mental_state(result_in_persent)
 
-    #     self.result = state
-
-    #     mental_state = MentalState.objects.filter(level=level).first()
-    #     self.employee.mental_state = mental_state
-    #     self.employee.save()
-
-        # костыль до появления калькулятора
-        mental_state = MentalState.objects.filter(level=1).first()
-        self.employee.mental_state = mental_state
-        self.employee.save()
-        self.mental_state = mental_state
         # еще костыль, для summary будет отдельная модель
         summary = None
+
         if self.survey.type.slug == 'mbi':
             # это для фронта, чтобы заранее делали шкалы
+            # https://psylab.info/Опросник_выгорания_Маслач
+            questions = [item['question_id'] for item in self.results]
+            questions = Question.objects.filter(id__in=questions)
+
+            ee = questions.filter(key=1).values_list('id', flat=True)
+            ee_max = 6 * ee.count() + 6
+            ee_sum = 0
+            dp = questions.filter(key=2).values_list('id', flat=True)
+            dp_max = 6 * dp.count()
+            dp_sum = 0
+            pa = questions.filter(key=3).values_list('id', flat=True)
+            pa_max = 6 * pa.count()
+            pa_sum = 0
+
+            # по методике 1 вариант инвертирован, ему даем ключ 4
+            inverted = questions.filter(key=4).values_list('id', flat=True)
+
+            for value in self.results:
+                if value['question_id'] in inverted:
+                    invert = 6 - value['variant_value']
+                    ee_sum += invert
+                if value['question_id'] in ee:
+                    ee_sum += value['variant_value']
+                if value['question_id'] in dp:
+                    dp_sum += value['variant_value']
+                if value['question_id'] in pa:
+                    pa_sum += value['variant_value']
+
+            index = sqrt(((ee_sum / ee_max)**2 + (dp_sum / dp_max)**2
+                          + (1 - pa_sum / pa_max)**2) / 3)
+            index = round(index, 2)
+
+            if 0 <= ee_sum <= 15:
+                ee_level = 1
+            elif 16 <= ee_sum <= 24:
+                ee_level = 2
+            else:
+                ee_level = 3
+
+            if 0 <= dp_sum <= 5:
+                dp_level = 1
+            elif 6 <= dp_sum <= 10:
+                dp_level = 2
+            else:
+                dp_level = 3
+
+            if 0 <= pa_sum <= 30:
+                pa_level = 3
+            elif 31 <= pa_sum <= 36:
+                pa_level = 2
+            else:
+                pa_level = 1
+
+            level = max([ee_level, dp_level, pa_level])
+
+            mental_state = MentalState.objects.filter(level=level).first()
+            self.mental_state = mental_state
+            self.employee.mental_state = mental_state
+            self.employee.save()
+
+            graphs_colors = {
+                1: 'green',
+                2: 'yellow',
+                3: 'red'
+            }
+
             summary = {
                 "graphs": [
-                    {"title": "Значение индекса системного выгорания",
-                     "type": "big", "percentage": 57, "value": 57},
-                    {"title": "Эмоциональное истощение",
-                     "type": "normal", "percentage": 89, "value": 48},
-                    {"title": "Деперсонализация",
-                     "type": "normal", "percentage": 66, "value": 20},
-                    {"title": "Редукция проф.достижений",
-                     "type": "normal", "percentage": 57, "value": 28},
+                    {
+                        "title": "Значение индекса системного выгорания",
+                        "size": "big", "color": "blue", "value": index,
+                        "min_value": 0, "max_value": 1
+                    },
+                    {
+                        "title": "Эмоциональное истощение",
+                        "size": "medium", "color": graphs_colors[ee_level],
+                        "value": ee_sum, "min_value": 0, "max_value": ee_max
+                    },
+                    {
+                        "title": "Деперсонализация",
+                        "size": "medium", "color": graphs_colors[dp_level],
+                        "value": dp_sum, "min_value": 0, "max_value": dp_max
+                    },
+                    {
+                        "title": "Редукция проф.достижений",
+                        "size": "medium", "color": graphs_colors[pa_level],
+                        "value": pa_sum, "min_value": 0, "max_value": pa_max
+                    },
                 ]
             }
+
         self.summary = summary
 
         if self.survey.frequency:
