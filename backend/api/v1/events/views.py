39b8109a2d2adf28ba9e8e5943b172a3ce212998
Aleksandr Usolcev <alexusolev@gmail.com>
2023-06-20 21:57:52 +0300

    feat(events): rework entries/categories

--- backend/api/v1/events/views.py
+++ backend/api/v1/events/views.py
@@ -1,21 +1,36 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from api.v1.permissions import (AllowAuthorOrReadOnly, ChiefPostPermission,
                                 ChiefSafePermission, EmployeeSafePermission,
                                 HRAllPermission)
-from events.models import Entry, Event
+from events.models import Category, Entry, Event
 
 from .filters import EntryFilter
-from .serializers import (EntryReadSerializer, EntryWriteSerializer,
-                          EventReadSerializer, EventWriteSerializer)
+from .serializers import (CategorySerializer, EntryReadSerializer,
+                          EntryWriteSerializer, EventReadSerializer,
+                          EventWriteSerializer)
 
 User = get_user_model()
 
 
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    http_method_names = ('get',)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CategorySerializer
+    pagination_class = None
+
+
 class EntryViewSet(ModelViewSet):
-    queryset = Entry.objects.select_related('category', 'author').all()
+    queryset = (
+        Entry.objects
+        .select_related('author')
+        .prefetch_related('category')
+        .all()
+    )
     filter_backends = (DjangoFilterBackend,)
     filterset_class = EntryFilter
     http_method_names = ('get', 'post', 'patch', 'delete')
