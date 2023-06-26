69a07e1b4f38c51f221bc228e62c022bb5071f2a
Ilia Steblyan <stebljan@ya.ru>
2023-06-26 21:13:44 +0600

    feat(email)!: created service for invites & emails
    Added templates for emails with invite codes and  notifications.

--- backend/conf/tasks.py
+++ backend/conf/tasks.py
@@ -12,33 +12,33 @@ def send_mail_via_celery(
     auth_user=None,
     auth_password=None,
     connection=None,
-    html_message=None,
+    html_message=None
 ):
     send_mail(
         subject,
         message,
         from_email,
         recipient_list,
-        fail_silently=False,
-        auth_user=None,
-        auth_password=None,
-        connection=None,
-        html_message=None,
+        fail_silently,
+        auth_user,
+        auth_password,
+        connection,
+        html_message=html_message,
     )
 
 
-@shared_task(name='mass_send_mail', ignore_result=True)
+@shared_task(name='send_mass_mail', ignore_result=True)
 def send_mass_mail_via_celery(
     datatuple,
     fail_silently=False,
     auth_user=None,
     auth_password=None,
-    connection=None,
+    connection=None
 ):
     send_mass_mail(
         datatuple,
-        fail_silently=False,
-        auth_user=None,
-        auth_password=None,
-        connection=None,
+        fail_silently,
+        auth_user,
+        auth_password,
+        connection,
     )
