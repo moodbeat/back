ce1e8b253c9aa499651fcb24fbb273d2f1b4955c
Ilia Steblyan <stebljan@ya.ru>
2023-07-15 13:58:47 +0600

    feat(bot)!: upgrade entries

--- bot/handlers/base.py
+++ bot/handlers/base.py
@@ -1,19 +1,21 @@
 from aiogram import Router, types
+from aiogram.filters import Text
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
-
 from config_reader import config
 from handlers.api_request import make_get_request
 from middlewares.auth import AuthMiddleware
-
 
 router = Router()
 
 router.message.middleware(AuthMiddleware())
 
 
+@router.callback_query(Text(startswith='back_start'))
 @router.message(Command('start'))
-async def cmd_start(message: types.Message, state: FSMContext):
+async def cmd_start(
+    message: types.Message | types.CallbackQuery, state: FSMContext
+):
     state = await state.get_data()
     headers = state.get('headers')
     response = await make_get_request(
@@ -26,14 +28,18 @@ async def cmd_start(message: types.Message, state: FSMContext):
         f'{response.get("first_name", message.from_user.first_name)}. '
         f'Я умею почти всё, что умеет сервис «Настроение сотрудника»: '
         f'cо мной Вы можете проходить опросы, '
-        f'отмечать своё состоянии в течение дня, '
+        f'отмечать своё состояние в течение дня, '
         f'читать полезные новости и статьи, '
         f'получать уведомления о проходящих мероприятиях.\n\n'
         f'Список доступных команд:\n'
-        f'/entries - Список доступных событий\n'
+        f'/entries - Список доступных статей\n'
         f'/events - Список актуальных мероприятий\n'
         f'/mood - Пройти опрос о своём состоянии\n'
         f'/survey - Список доступных опросов\n'
         f'/need_help - Отправить запрос на личную встречу со специалистом'
     )
-    await message.answer(start_msg)
+
+    if isinstance(message, types.CallbackQuery):
+        await message.message.delete()
+    else:
+        await message.answer(start_msg)
